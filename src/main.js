0194f700971bb22d8d8148167052d43b28018c7a
Matvey <matveym151@gmail.com>
2025-01-25 14:39:31 +0300

    deploy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from "@/router/router.js";
+import directives from "@/directives";
+import {autoAnimatePlugin} from "@formkit/auto-animate/vue";
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import './index.scss';
+import { VueQueryPlugin  } from "vue-query";
+// import { ValidationProvider } from 'vee-validate';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+
+directives.forEach((directive) => {
+  app.directive(directive.name, directive);
+});
+
+// app.component('ValidationProvider', ValidationProvider);
+
+app
+  .use(autoAnimatePlugin)
+  .use(pinia)
+  .use(router)
+  .use(PrimeVue, { unstyled: true })
+  .use(VueQueryPlugin )
+  .mount('#app')
